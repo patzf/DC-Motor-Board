12d230a32b3a947993ae79c2a788b9c8172dc531
Franz Patz <franz.patz@gmx.at>
2026-09-10 10:01:39 +0200

    rigol capture

--- fw/Nucleo_serial.py
+++ fw/Nucleo_serial.py
@@ -1,30 +1,4 @@
-import serial
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
+import pyvisa
 
-with serial.Serial('COM3', 115200, timeout=None) as ser:
-
-    ser.flushInput()
-
-    raw = ser.read(2000)
-
-    # Convert 800 bytes -> 200 floats
-    values = np.frombuffer(raw, dtype='<f4')
-
-    # Save to CSV
-    with open('data.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(values)
-
-    print("written raw data to csv")
-
-    # Time axis: 1 ms per sample
-    t = np.arange(len(values))  # 0, 1, 2, ... ms
-
-    # Plot
-    plt.plot(t, values)
-    plt.xlabel("Time [ms]")
-    plt.ylabel("RPM")
-    plt.grid()
-    plt.show()
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
